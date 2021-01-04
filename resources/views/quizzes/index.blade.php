8a32709fb3a121e681807baa34a36f962b82b4f3
Masashi Tsuru <1150769+masa0221@users.noreply.github.com>
2021-01-05 08:34:01 +0900

    quiz管理トップのページを追加

---
+++ resources/views/quizzes/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quiz 管理</title>
+</head>
+<body>
+    <span>Quiz管理トップページ</span>
+</body>
+</html>
